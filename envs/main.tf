2a6c438b7dd3e56f508e8fdf96f2ae03887b82ba
kyosu-1 <sho013039@gmail.com>
2023-09-21 10:18:39 +0900

    feat: apply

--- envs/main.tf
+++ envs/main.tf
@@ -3,3 +3,37 @@ module "sushi-order-system-api-ecr" {
     
     app_name       = local.app_name
 }
+
+module "vpc" {
+  source = "../modules/vpc"
+
+  app_name = local.app_name
+
+  vpc_cidr = "172.16.0.0/16"
+  public_subnets = {
+    public_1a = { cidr_block = "172.16.1.0/24", az = "ap-northeast-1a" },
+    public_1c = { cidr_block = "172.16.2.0/24", az = "ap-northeast-1c" },
+    public_1d = { cidr_block = "172.16.3.0/24", az = "ap-northeast-1d" },
+  }
+  apprunner_subnets = {
+    apprunner_1a = { cidr_block = "172.16.11.0/24", az = "ap-northeast-1a" },
+    apprunner_1c = { cidr_block = "172.16.12.0/24", az = "ap-northeast-1c" },
+    apprunner_1d = { cidr_block = "172.16.13.0/24", az = "ap-northeast-1d" },
+  }
+  db_subnets = {
+    db_1a = { cidr_block = "172.16.21.0/24", az = "ap-northeast-1a" },
+    db_1c = { cidr_block = "172.16.22.0/24", az = "ap-northeast-1c" },
+    db_1d = { cidr_block = "172.16.23.0/24", az = "ap-northeast-1d" },
+  }
+  natgw_subnet_name   = "public_1a"
+  bastion_subnet_name = "public_1a"
+}
+
+module "sg" {
+  source = "../modules/sg"
+
+  app_name = local.app_name
+
+  db_port    = local.db_port
+  vpc_id     = module.vpc.vpc.id
+}
--- envs/variables.tf
+++ envs/variables.tf
@@ -1,3 +1,11 @@
 locals {
   app_name       = "sushi-order-system"
 }
+
+// DB
+locals {
+  db_port        = "3306"
+  db_name        = "sushi"
+  db_username    = "sushi"
+  db_password    = "tmp-password" # 一時的なパスワード(後で変更)
+}
